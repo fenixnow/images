addb316da07636477a6e467238d712aeb1a1d497
Fenixnow <fenixnow@mail.ru>
2023-05-12 11:55:56 +0300

    Update emulator-snapshot.sh

--- selenium/android/emulator-snapshot.sh
+++ selenium/android/emulator-snapshot.sh
@@ -7,8 +7,8 @@ adb devices | grep emulator | cut -f1 | while read id; do
         if [ -r "$apk" ]; then
             for i in `seq 1 ${MAX_ATTEMPTS}`; do
                 echo "Installing $apk (attempt #$i of $MAX_ATTEMPTS)"
-                adb -s "$id" install -r "$apk" && break || sleep 15 && echo "Retrying to install $apk"
-                adb shell am start -n ru.vkusvill/ru.vkusvill.ui.screens.splash.SplashActivity || sleep 30
+                adb -s "$id" install -r "$apk" && break || sleep 60 && echo "Retrying to install $apk"
+                adb shell am start -n ru.vkusvill/ru.vkusvill.ui.screens.splash.SplashActivity && break || sleep 30
                 adb shell am force-stop ru.vkusvill
             done
         fi
