9889fddb6c96abd1b0a61d9729ce2cc31cd7adc9
Leonid Vanin <vanl@automacon.ru>
2023-05-24 12:10:00 +0300

    +

--- selenium/android/entrypoint.sh
+++ selenium/android/entrypoint.sh
@@ -75,7 +75,7 @@ fi
 while [ "$(adb shell getprop sys.boot_completed | tr -d '\r')" != "1" ] && [ -z "$STOP" ] ; do sleep 1; done
 if [ -n "$STOP" ]; then exit 0; fi
 
-DEFAULT_CAPABILITIES=''
+DEFAULT_CAPABILITIES='"platformName": "Android", "automationName": "uiautomator2"'
 if [ -n "@CHROME_MOBILE@" ]; then
     while ip addr | grep inet | grep -q tentative > /dev/null; do sleep 0.1; done
     DEFAULT_CAPABILITIES=$DEFAULT_CAPABILITIES', "chromedriverPort": '$CHROMEDRIVER_PORT
@@ -87,7 +87,7 @@ if [ -x "/usr/bin/chromedriver" ]; then
     DEFAULT_CAPABILITIES=$DEFAULT_CAPABILITIES',"chromedriverExecutable": "/usr/bin/chromedriver"'
 fi
 
-appium --base-path=/wd/hub -p "$PORT" -cp "$BOOTSTRAP_PORT" --log-timestamp --log-no-colors ${APPIUM_ARGS} --default-capabilities {$DEFAULT_CAPABILITIES} &
+appium -p "$PORT" -cp "$BOOTSTRAP_PORT" --log-timestamp --log-no-colors ${APPIUM_ARGS} --default-capabilities {$DEFAULT_CAPABILITIES} &
 APPIUM_PID=$!
 
 wait
